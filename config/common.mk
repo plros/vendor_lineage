ae7db6f55fc153fe94a92e6856574db64a9fd6b6
Whot1966 <Whot1966@users.noreply.github.com>
2023-07-31 16:59:17 +0000

    common: Inherit new plros vendor

--- config/common.mk
+++ config/common.mk
@@ -221,3 +221,6 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+include vendor/plros/plros.mk
+include vendor/plros/config/version.mk
